0624052c29ad135809d7bb0d59831100470b1b34
Baran Dolapci <72374750+bdolapci@users.noreply.github.com>
2021-02-13 21:55:36 +0300

    Various updates

--- public/hb.js
+++ public/hb.js
@@ -1,10 +1,12 @@
 const hb = document.querySelector(".header .navbar .navlist .hb");
 const mobile = document.querySelector(".header .navbar .navlist ul");
+const mobile2 = document.querySelector(".Welcome");
 const header = document.querySelector(".header.container");
 
 hb.addEventListener("click", () => {
   hb.classList.toggle("active");
   mobile.classList.toggle("active");
+  mobile2.classList.toggle("spanwelcome");
 });
 document.addEventListener("scroll", () => {
   var sp = window.scrollY;
